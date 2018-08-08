74ddefac17fe6f2057c52138af523f0012df7971
Franklin Parker <fparker@deliveryman.com>
2018-08-08 17:26:44 -0400

    validate

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Field, reduxForm} from 'redux-form'
 import {BwmInput} from "../shared/form/BwmInput";
 import {BwmResError} from "../shared/form/BwnResError";
+import { minLength4, required} from "../shared/form/validate";
 
 
 const LoginForm = props => {
@@ -15,12 +16,14 @@ const LoginForm = props => {
         label='Email'
         className='form-control'
         component={BwmInput}
+        validate={[required, minLength4]}
       />
       <Field
         name="password"
         type="password"
         label='Password'
         className='form-control'
+        validate={[required]}
         component={BwmInput}
       />
       <button className='btn btn-bwm btn-form' type="submit" disabled={!valid || pristine || submitting}>
@@ -32,23 +35,7 @@ const LoginForm = props => {
   )
 }
 
-const validate = values => {
-  const errors = {}
-
-  if (!values.email) {
-    errors.email = 'Please enter email'
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Please enter valid email'
-  }
-
-
-  if (!values.password) {
-    errors.password = 'Password  Required'
-  }
-  return errors
-}
 
 export default reduxForm({
-  form: 'loginForm',
-  validate
+  form: 'loginForm'
 })(LoginForm)
