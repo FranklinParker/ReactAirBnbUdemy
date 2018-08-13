faae759a3709e2bff82dc65a74fb98ea7dfaa0c4
Franklin Parker <fparker@deliveryman.com>
2018-08-13 13:21:05 -0400

    booking

--- src/components/booking/Booking.js
+++ src/components/booking/Booking.js
@@ -1,10 +1,65 @@
 import React from 'react';
 import DateRangePicker from 'react-bootstrap-daterangepicker';
+import { getRangeOfDates} from "../../helpers";
+import * as moment from 'moment';
 
 export class Booking extends React.Component {
 
+  constructor(){
+    super();
+    this.dateRef = React.createRef();
+    this.bookedOutDates = [];
+    this.state = {
+      startAt:'',
+      endAt: '',
+      guests: 0
+    }
+    this.checkInvalidDate = this.checkInvalidDate.bind(this);
+    this.handleApply = this.handleApply.bind(this);
+  }
+
+  componentWillMount(){
+    this.getBookedOutDates();
+  }
+
+  getBookedOutDates(){
+    const { bookings } = this.props.rental;
+    if(bookings && bookings.length > 0){
+      bookings.forEach(booking=> {
+        let bookedDates = getRangeOfDates(booking.startAt, booking.endAt, 'Y/MM/DD');
+        this.bookedOutDates.push(...bookedDates);
+      });
+
+    }
+
+  }
+
+  handleApply(event, picker){
+    const startAt = picker.startDate.format('Y/MM/DD');
+    const endAt = picker.endDate.format('Y/MM/DD');
+    this.dateRef.current.value = startAt + ' to ' + endAt;
+
+    this.setState( {
+      startAt,
+       endAt
+     });
+  }
+
+  selectGuests(event){
+    this.setState({
+      guests: parseInt(event.target.value,10)
+    });
+  }
+  checkInvalidDate(date){
+    return this.bookedOutDates.includes(date.format('Y/MM/DD')) || date.diff(moment(),'days')<-1;
+  }
+
+  reserve(){
+    console.log(this.state);
+  }
   render() {
     const { rental } = this.props;
+
 
     return (
       <div className='booking'>
@@ -12,15 +67,24 @@ export class Booking extends React.Component {
         <hr></hr>
         <div className='form-group'>
           <label htmlFor='dates'>Dates</label>
-          <DateRangePicker startDate="1/1/2014" endDate="3/1/2014">
-            <button>Select Date</button>
+          <DateRangePicker opens='left'
+                           onApply={this.handleApply}
+                           isInvalidDate={this.checkInvalidDate}
+                           containerStyles={{display: 'block'}} >
+           <input ref={this.dateRef} id='dates' type='text' className='form-control'/>
           </DateRangePicker>
         </div>
         <div className='form-group'>
           <label htmlFor='guests'>Guests</label>
-          <input type='number' className='form-control' id='guests' aria-describedby='emailHelp' placeholder=''></input>
+          <input type='number'
+                 onChange={(event)=>{this.selectGuests(event)}}
+                 className='form-control'
+                 id='guests'
+                 aria-describedby='emailHelp'
+                 placeholder=''></input>
         </div>
-        <button className='btn btn-bwm btn-confirm btn-block'>Reserve place now</button>
+        <button className='btn btn-bwm btn-confirm btn-block'
+                onClick={()=>{this.reserve()}}>Reserve place now</button>
         <hr></hr>
         <p className='booking-note-title'>People are interested into this house</p>
         <p className='booking-note-text'>
