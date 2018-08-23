8cdb0723595c4f129691a96cf874b8e7dc09828b
Franklin Parker <fparker@deliveryman.com>
2018-08-23 10:27:34 -0400

    manage booking

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import {ProtectedRoute} from "./components/shared/auth/ProtectedRoute";
 import {LoggedInRoute} from "./components/shared/auth/LoggedInRoute";
 import RentalSearchListing from "./components/rental/rental-listing/RentalSearchListing";
 import {RentalCreate} from "./components/rental/rental-create/RentalCreate";
+import {BookingManage} from "./components/booking/booking-manage/BookingManage";
+import {RentalManage} from "./components/rental/rental-manage/RentalManage";
 
 const store = require('./reducers').init();
 
@@ -42,8 +44,11 @@ class App extends Component {
                 <LoggedInRoute exact path="/register" component={Register}/>
                 <Route exact path="/rentals" component={RentalListing}/>
                 <Route exact path="/rentals/:city/homes" component={RentalSearchListing}/>
+                <ProtectedRoute exact path="/rentals/manage" component={RentalManage}/>
                 <ProtectedRoute exact path="/rentals/new" component={RentalCreate}/>
                 <ProtectedRoute exact path="/rentals/:id" component={RentalDetail}/>
+                <ProtectedRoute exact path="/bookings/manage" component={BookingManage}/>
+
               </Switch>
 
             </div>
---
+++ src/components/booking/booking-manage/BookingManage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export class BookingManage extends React.Component{
+  render(){
+    return (
+      <h1>I Booking manager</h1>
+    );
+  }
+}
