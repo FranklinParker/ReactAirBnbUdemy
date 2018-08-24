70c11c16218af8cc446989866e843b4421a3aebc
Franklin Parker <fparker@deliveryman.com>
2018-08-24 10:38:16 -0400

    check in

--- src/components/booking/booking-manage/BookingManage.js
+++ src/components/booking/booking-manage/BookingManage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from "react-redux";
 import * as actions from "../../../actions/index";
+import { Link} from 'react-router-dom';
 
 
 class BookingManage extends React.Component{
@@ -10,11 +11,41 @@ class BookingManage extends React.Component{
   render(){
     const { userBookings} = this.props;
     return (
-      <div>
-        { userBookings.data.map((booking,index)=>{
-           return (<p key={index}> {booking.startAt} - {booking.endAt} </p>)
-        })}
-      </div>
+      <section id='userBookings'>
+        <h1 className='page-title'>My Bookings</h1>
+        <div className='row'>
+
+          { userBookings.data.map((booking,index)=>
+            <div className='col-md-4' key={index}>
+              <div className='card text-center'>
+                <div className='card-header'>
+                  {booking.rental.category}
+                </div>
+                <div className='card-block'>
+                  <h4 className='card-title'> {booking.rental.title} - {booking.rental.city}</h4>
+                  <p className='card-text booking-desc'>{booking.rental.description}</p>
+                  <p className='card-text booking-days'>{booking.startAt} - {booking.endAt } | {booking.days} days</p>
+                  <p className='card-text booking-price'><span>Price: </span>
+                    <span className='booking-price-value'>{booking.totalPrice} $</span>
+                  </p>
+                  <Link className='btn btn-bwm'
+                        to={`/rentals/${booking.rental._id}`}>Go to Rental</Link>
+                </div>
+                <div className='card-footer text-muted'>
+                  Created {booking.created}
+                </div>
+              </div>
+            </div>
+
+          )}
+        </div>
+        <div className='alert alert-warning'>
+          You have no bookings created go to rentals section and book your place today.
+          <Link style={{'marginLeft': '10px'}} className='btn btn-bwm' to='rentals index page'>Available Rental</Link>
+        </div>
+      </section>
+
+
     );
   }
 }
